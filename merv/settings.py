c5cd39888a545366acbcf1391b403daedfcd36b7
Michael Tran <trankmichael@gmail.com>
2016-04-17 17:05:26 -0400

    fixed migrations

--- merv/settings.py
+++ merv/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.core.urlresolvers',
+    #'django.core.urlresolvers', <<  ------ breaks migrations
 
     # our apps
     'accounts',
