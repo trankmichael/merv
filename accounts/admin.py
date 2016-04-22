84aad0dfe3515112c20535bc4fdc156b57dd7c6a
Michael Tran <trankmichael@gmail.com>
2016-04-21 23:10:17 -0400

    added ratings model and updated gitignore to include migrations

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from tasks.models import Task
 from accounts.models import User
+from recommender.models import Ratings
 # Register your models here.
 admin.site.register(Task)
 admin.site.register(User)
+admi.site.register(Ratings)
--- recommender/models.py
+++ recommender/models.py
@@ -1,5 +1,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from accounts.models import User 
+from tasks.models import Task
 
 # Create your models here.
+class Ratings(models.Model)
+    """A Vote on a Product"""
+    user = models.ForeignKey(User, related_name='ratings')
+    task = models.ForeignKey(Task)
+    score = models.FloatField()
+
+    def __str__(self):
+        return "Vote"
