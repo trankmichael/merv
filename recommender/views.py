73ad72715b4d536295db67843ba2327219c3400b
Michael Tran <trankmichael@gmail.com>
2016-04-22 00:47:45 -0400

    removed comments from recommender.views

--- recommender/views.py
+++ recommender/views.py
@@ -16,41 +16,9 @@ class RatingCreateUpdate(CreateView):
 		form.instance.task_pk = task_pk
 		return super(RatingCreateUpdate, self).form_valid(form)
 
-	# def get(self, request, *args, **kwargs):
-	# 	queryset = Rating.objects.filter(user=self.request.user, task=self.kwargs['pk'])
-	# 	if queryset.exists():
-	# 		self.object = self.get_object()
-	# 		return super(BaseUpdateView, self).get(request, *args, **kwargs)
-	# 	else:
-	# 		self.object = None
-	# 		return super(BaseCreateView, self).get(request, *args, **kwargs)
-
 	def post(self, request, *args, **kwargs):
 		Rating.objects.filter(user=self.request.user, task=self.kwargs['pk']).delete()
 		self.object = None
 		return super(BaseCreateView, self).post(request, *args, **kwargs)
-		# if queryset.exists():
-		# 	#update rating if it already exists 
-		# 	self.object = self.get_object()
-		# 	return super(BaseUpdateView, self).post(request, *args, **kwargs)
-		# else:
-		# 	#create rating if it doesnt
-		# 	self.object = None
-		# 	return super(BaseCreateView, self).post(request, *args, **kwargs)
 
-	# def get_object(self, queryset=None):
-	# 	queryset = Rating.objects.filter(user=self.request.user, task=self.kwargs['pk'])
-	# 	print queryset
-	# 	try:
-	# 		return super(RatingCreateUpdate,self).get_object(queryset)
-	# 	except AttributeError:
-	# 		return None
-
-	# def get(self, request, *args, **kwargs):
-	# 	self.object = self.get_object()
-	# 	return super(RatingCreateUpdate, self).get(request, *args, **kwargs)
-
-	# def post(self, request, *args, **kwargs):
-	# 	self.object = self.get_object()
-	# 	return super(RatingCreateUpdate, self).post(request, *args, **kwargs)
 
